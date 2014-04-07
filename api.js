ee36d3cb47b3c42f82f8f97f97c28573fe72a7e5
Daniel Werner <daniel.werner@priologic.com>
2014-04-07 09:07:44 -0700

    First working rev of client-side API

--- api.js
+++ api.js
@@ -4,16 +4,18 @@
 
 	/*utils*/
 	function assert(condition, failureMessage) { if (!condition) { throw new Error("Assert failed: " + message); } }
-	
+
 	/*
 	*	addEventHandler(id, functionName, handler)
 	*	Use eval for the power of good. (Bind event handler to native C++ event)
 	*/
-	function addEventHandler(id, handler) {
+	function addEventHandler(context, id, handler) {
 		try {
 			
 			// Eval bind double-colon function to call handler
-			window.callback_registry[id+"::EventToBrowser"] = handler;
+			window.callback_registry[id+"::EventToBrowser"] = function (event) {
+				handler.call(context, [event]);
+			};
 
 			var evalString = 
 				"function " + id + "::EventToBrowser (event) { "+
@@ -26,7 +28,7 @@
 
 		} catch (e) {
 			console.log(e);
-			throw new Error("Could not add custom event handler to "+ id);
+			console.log("Could not add custom event handler to "+ id);
 		}
 	}
 
@@ -35,60 +37,116 @@
 	*/
 	function RTCPlugin(activexElement) {
 		this.element = activexElement;
-		addEventHandler(activexElement.id, this.nativeEventHandler);
+		addEventHandler(this, activexElement.id, this.nativeEventHandler);
 		this.remoteId = null;
+		this.isRunning = false;
 	}
 
-	RTCPlugin.prototype.onicecandidate = function (){ console.log("onicecandidate"); };
+	RTCPlugin.prototype.onicecandidate = function (json){ console.log("onicecandidate"); };
 
 	/*
 	* RTCPlugin interface implementation
 	*/
 	RTCPlugin.prototype.createOffer = function (remoteId, success) {
 		this.remoteId = remoteId;
-		this.element.pushToNative('makeoffer', '');
 		this.onCreateOffer = success;
+		nativeCall(this, 'makeoffer', '');
+
 	};
 
-	RTCPlugin.prototype.handleOffer = function (offer, success) {
-		this.element.pushToNative('handleoffer', JSON.stringify(offer));
-		this.onHandleOffer = success;
+	RTCPlugin.prototype.handleOffer = function (remoteId, offer, success) {
+		if (!this.handlingOffer) {
+			this.handlingOffer = true;
+			this.remoteId = remoteId;
+			this.onHandleOffer = success;
+			nativeCall(this, 'handleoffer', offer);
+		} else {
+			throw new Error ("Already handling an offer!");
+		}
+
 	};
 
-	RTCPlugin.prototype.handleAnswer = function (answer, success) {
-		this.element.pushToNative('handleanswer', JSON.stringify(answer));
+	RTCPlugin.prototype.handleAnswer = function (answer) {
+		nativeCall(this, 'handleanswer', JSON.stringify(answer));
 	};
 
 	RTCPlugin.prototype.handleCandidate = function (candidate) {
-		this.element.pushToNative("handlecandidate", JSON.stringify(candidate));
+		nativeCall(this, "handlecandidate", JSON.stringify(candidate));
 	};
 
 	RTCPlugin.prototype.close = function () {
-		this.element.pushToNative("hangup", '');
+		nativeCall(this, "hangup", '');
 	};
 
-	RTCPlugin.prototype.debugNative = function () {
-		this.element.pushToNative("debug", '');
+	RTCPlugin.prototype.nativeDebug = function () {
+		nativeCall(this, "debug", '');
 	};
 
 	RTCPlugin.prototype.run = function () {
-		this.element.run();
+		if (!this.isRunning) {
+			this.isRunning = true;
+			this.element.run();
+		} else {
+			alert("RTCPlugin.run() called twice!");
+		}
 	};
 
-	RTCPlugin.prototype.nativeEventHandler = function (json) {
+	RTCPlugin.prototype.nativeEventHandler = function (data) {
 
-		if (json.type === 'offer' && this.onCreateOffer) {
+		var json;
+		var obj;
+		if (typeof data == 'string'){
+			dlog("(.)(.) == dont treat me like an object!");
+			json = json[0];
+			json = JSON.parse(data);
+			dlog(json);
+		} else {
+			dlog("(.)(.)object");
+			json = data;
+			for (var i in json){
+				dlog(i + " : " + json[i]);
+			}
+			obj = json[0];
+			dlog(">>>> obj :" + obj )
+			json = JSON.parse(obj);
+		}
+
+	
+		dlog(this.onCreateOffer !== undefined);
+		dlog(this.onHandleOffer !== undefined);
+		dlog(this.onicecandidate !== undefined);
+		dlog("json.type:"+json.type);
+		dlog("json.candidate:"+json.candidate);
+
+
+		if (json.type == 'offer' && this.onCreateOffer) {
+			dlog("offer from native");
 			this.onCreateOffer(json.sdp);
 		
-		} else if (json.type === 'answer' && this.onHandleOffer) { //back from C++
+		} else if (json.type == 'answer' && this.onHandleOffer) { //back from C++
+
 			this.onHandleOffer(json.sdp);
+			this.handlingOffer = false;
 		
 		} else if(json.candidate && this.onicecandidate) {
+			dlog('candidate from native');
 			this.onicecandidate(json);
+		} else {
+			dlog("default case (called from native, but we dont know why)");
+			dlog(json);
 		}
 	}
+
+	function nativeCall(plugin, msg, data) {
+		dlog("native call: " + msg + " data:" + (typeof data === 'object' ? JSON.stringify(data) : data));
+		if (plugin.isRunning){
+			plugin.element.pushToNative(msg, typeof data === 'object' ? JSON.stringify(data) : data);
+		}
+	}
+
 
 	window.RTCPlugin = RTCPlugin;
 
 })(window, document);
+
 
